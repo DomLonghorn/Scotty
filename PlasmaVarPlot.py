38904f0e3edbfd89ec2cd5f3cf7524f7316b2c41
Dom Longhorn <dpl508@york.ac.uk>
2022-11-16 15:54:26 +0000

    Plotted ray propagation

--- PlasmaVarPlot.py
+++ PlasmaVarPlot.py
@@ -26,10 +26,14 @@ electron_density_output = loadfile["electron_density_output"]
 B_magnitude = loadfile["B_magnitude"]
 b_hat_output = loadfile["b_hat_output"]
 x_hat_output = loadfile["x_hat_output"]
+q_R_array = loadfile["q_R_array"]
+q_zeta_array = loadfile["q_zeta_array"]
+q_Z_array = loadfile["q_Z_array"]
 loadfile.close()
 
 # Loads in input conditions
 loadfile = np.load("data_input" + suffix + ".npz")
+launch_position = loadfile["launch_position"]
 data_R_coord = loadfile["data_R_coord"]
 data_Z_coord = loadfile["data_Z_coord"]
 poloidalFlux_grid = loadfile["poloidalFlux_grid"]
@@ -40,29 +44,36 @@ B_R = B_magnitude * b_hat_output[:, 0]
 B_T = B_magnitude * b_hat_output[:, 1]
 B_Z = B_magnitude * b_hat_output[:, 2]
 
-# plt.title("Poloidal Plane")
-# contour_levels = np.linspace(0, 1, 11)
-# CS = plt.contour(
-#     data_R_coord,
-#     data_Z_coord,
-#     np.transpose(poloidalFlux_grid),
-#     contour_levels,
-#     vmin=0,
-#     vmax=1.2,
-#     cmap="inferno",
-# )
-# plt.clabel(
-#     CS, inline=True, fontsize=10, inline_spacing=-5, fmt="%1.1f", use_clabeltext=True
-# )  # Labels the flux surfaces
-# plt.xlim(1.0,1.8)
-# plt.ylim(-0.7,0.1)
-# tikzplotlib.clean_figure() # Removes points that are outside the plot area
-# plt.plot(q_R_array[:out_index], q_Z_array[:out_index], "k")
-# plt.plot([launch_position[0], q_R_array[0]], [launch_position[2], q_Z_array[0]], ":k")
-# plt.plot(W_line_RZ_1_Rpoints[:out_index], W_line_RZ_1_Zpoints[:out_index], "k--")
-# plt.plot(W_line_RZ_2_Rpoints[:out_index], W_line_RZ_2_Zpoints[:out_index], "k--")
-# plt.xlim(data_R_coord[0], data_R_coord[-1])
-# plt.ylim(data_Z_coord[0], data_Z_coord[-1])
+## For plotting how the beam propagates from launch to entry
+launch_position_X, launch_position_Y, launch_position_Z = find_q_lab_Cartesian(
+    launch_position
+)
+entry_position_X, entry_position_Y, entry_position_Z = find_q_lab_Cartesian(
+    np.array([q_R_array[0], q_zeta_array[0], q_Z_array[0]])
+)
+
+numberOfDataPoints = np.size(q_R_array)
+out_index = numberOfDataPoints
+
+plt.title("Poloidal Plane")
+contour_levels = np.linspace(0, 1, 11)
+CS = plt.contour(
+    data_R_coord,
+    data_Z_coord,
+    np.transpose(poloidalFlux_grid),
+    contour_levels,
+    vmin=0,
+    vmax=1.2,
+    cmap="inferno",
+)
+plt.clabel(
+    CS, inline=True, fontsize=10, inline_spacing=-5, fmt="%1.1f", use_clabeltext=True
+)  # Labels the flux surfaces
+
+plt.plot(q_R_array[:out_index], q_Z_array[:out_index], "k")
+plt.plot([launch_position[0], q_R_array[0]], [launch_position[2], q_Z_array[0]], ":k")
+plt.xlim(data_R_coord[0], data_R_coord[-1])
+plt.ylim(data_Z_coord[0], data_Z_coord[-1])
 plt.xlabel("R / m")
 plt.ylabel("Z / m")
-plt.show()
+plt.savefig("Ray_Propagation.jpg")
